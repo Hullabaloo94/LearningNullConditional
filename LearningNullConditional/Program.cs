2993ae0c0d6aa5e26931dd87f6c15235c6e5b6f7
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:34:37 +0000

    initial commit

---
+++ LearningNullConditional/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningNullConditional
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();// { Name = "Tim" };
+            // The ? after person (code below) is stating that the person variable (for this statement only) can be null so don't fire a NullReferenceException if it is null. 
+            // Combining Null Conditional (person?) with Null Coalescing (??) we are able to access a person variable that may be null, try and access its name and if that is null then output "Not Set", otherwise output the name.
+            Console.WriteLine(person?.Name ?? "Not Set.");
+        }
+    }
+}
